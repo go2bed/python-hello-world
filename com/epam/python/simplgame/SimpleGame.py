481237afd9d60056446b72a2d2a6e5f1bd035a4b
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-09-05 16:38:02 +0600

    [IMP] Goblin hitting

--- com/epam/python/simplgame/SimpleGame.py
+++ com/epam/python/simplgame/SimpleGame.py
@@ -1,3 +1,64 @@
+class GameObject:
+    class_name = ""
+    desc = ""
+    objects = {}
+
+    def __init__(self, name):
+        self.name = name
+        GameObject.objects[self.class_name] = self
+
+    def get_desc(self):
+        return self.class_name + "\n" + self.desc
+
+
+class Goblin(GameObject):
+    def __init__(self, name):
+        self.class_name = "goblin"
+        self.health = 3
+        self._desc = "A foul creature"
+        super().__init__(name)
+
+    @property
+    def desc(self):
+        if self.health >= 3:
+            return self._desc
+        elif self.health == 2:
+            health_line = "It has a wound on its knee."
+        elif self.health == 1:
+            health_line = "Its left arm has been cut off!"
+        elif self.health <= 0:
+            health_line = "It is dead."
+        return self._desc + "\n" + health_line
+
+    @desc.setter
+    def desc(self, value):
+        self._desc = value
+
+
+goblin = Goblin("Gobbly")
+
+
+def hit(noun):
+    if noun in GameObject.objects:
+        thing = GameObject.objects[noun]
+        if type(thing) == Goblin:
+            thing.health -= 1
+            if thing.health <= 0:
+                msg = "You killed the goblin!"
+            else:
+                msg = "You hit the {}".format(thing.class_name)
+    else:
+        msg = 'There is no {} here'.format(noun)
+    return msg
+
+
+def examine(noun):
+    if noun in GameObject.objects:
+        return GameObject.objects[noun].get_desc()
+    else:
+        return "There is no {} here".format(noun)
+
+
 def get_input():
     command = input(": ").split()
     verb_word = command[0]
@@ -21,35 +82,7 @@ def say(noun):
     return 'You said "{}"'.format(noun)
 
 
-class GameObject:
-    class_name = ""
-    desc = ""
-    objects = {}
-
-    def __init__(self, name):
-        self.name = name
-        GameObject.objects[self.class_name] = self
-
-    def get_desc(self):
-        return self.class_name + "\n" + self.desc
-
-
-class Goblin(GameObject):
-    class_name = "goblin"
-    desc = "A foul creature"
-
-
-goblin = Goblin("Gobbly")
-
-
-def examine(noun):
-    if noun in GameObject.objects:
-        return GameObject.objects[noun].get_desc()
-    else:
-        return "There is no {} here".format(noun)
-
-
-verb_dict = {"say": say, "examine": examine, "exit": exit}
+verb_dict = {"say": say, "examine": examine, "hit": hit, "exit": exit}
 
 main = True
 
