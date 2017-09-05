862cf75b7cf4f61dddd42a9679795084456f25d4
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-09-05 14:08:35 +0600

    [REF] Added goblin creature(class)

--- com/epam/python/simplgame/SimpleGame.py
+++ com/epam/python/simplgame/SimpleGame.py
@@ -8,7 +8,7 @@ def get_input():
         return
 
     if verb_word == "exit":
-       main = False
+        main = False
 
     if len(command) >= 2:
         noun_word = command[1]
@@ -21,10 +21,37 @@ def say(noun):
     return 'You said "{}"'.format(noun)
 
 
-verb_dict = {"say": say, "exit": exit}
+class GameObject:
+    class_name = ""
+    desc = ""
+    objects = {}
+
+    def __init__(self, name):
+        self.name = name
+        GameObject.objects[self.class_name] = self
+
+    def get_desc(self):
+        return self.class_name + "\n" + self.desc
+
+
+class Goblin(GameObject):
+    class_name = "goblin"
+    desc = "A foul creature"
+
+
+goblin = Goblin("Gobbly")
+
+
+def examine(noun):
+    if noun in GameObject.objects:
+        return GameObject.objects[noun].get_desc()
+    else:
+        return "There is no {} here".format(noun)
+
+
+verb_dict = {"say": say, "examine": examine, "exit": exit}
 
 main = True
-
 
 while main:
     get_input()
