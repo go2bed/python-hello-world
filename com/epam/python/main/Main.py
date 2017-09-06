9bafda230c3f614d44947ad75c711952e943c10a
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-09-06 18:08:49 +0600

    [REF] some Alignment

--- com/epam/python/main/Main.py
+++ com/epam/python/main/Main.py
@@ -1,11 +1,13 @@
-def calc(a,b):
-    count=5
+def calc(a, b):
+    count = 5
     try:
-        count+=a/b
+        count += a / b
     except TypeError:
-        count-=4
+        count -= 4
         print("type error")
     except:
-        count+=27
+        count += 27
     return count
+
+
 print(str(calc("6", 4)))
